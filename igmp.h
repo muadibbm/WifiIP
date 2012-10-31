4fdf8b798722a61206f2da15c6de364c289f91df
Mehrdad <muadibbm@yahoo.com>
2012-10-31 19:02:15 -0400

    added IGMP src and header files

---
+++ igmp.h
@@ -0,0 +1,16 @@
+
+#include "protocol.h"
+#include "ip.h"
+#include "message.h"
+#include "grouter.h"
+
+#include <slack/err.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+/* TODO : any global variables and structs go here */
+
+/* TODO : prototype methods from igmp.c should be inserted here */
