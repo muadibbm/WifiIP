8d684471115e9c1b22b2b0988bd5409b8a1278b3
Andrey <akpaunov@gmail.com>
2012-11-01 14:56:09 -0400

    Added the structure for message request

--- igmp.c
+++ igmp.c
@@ -1,6 +1,13 @@
 
 #include "igmp.h"
 
+
+struct ip_mreq
+{
+	struct in_addr imr_multiaddr;   /* IP multicast address of group */
+	struct in_addr imr_interface;   /* local IP address of interface */
+};
+
 //function to receive the datagram with IP multicast "1110"
 
 //function to check the TTL, compare it with the router threshold value; if acceptabe increment the TTL and call forwarding function
