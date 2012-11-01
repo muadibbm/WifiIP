ec1fd3e418b7a348d8117c7f295100133710bd33
Mehrdad <muadibbm@yahoo.com>
2012-11-01 17:25:55 -0400

    merged

--- igmp.c
+++
@@ -1,9 +0,0 @@
-
-#include "igmp.h"
-
-
-//function to receive the datagram with IP multicast "1110"
-
-//function to check the TTL, compare it with the router threshold value; if acceptabe increment the TTL and call forwarding function
-
-//forwarding function checks which groups the multicast will be forwarded to
